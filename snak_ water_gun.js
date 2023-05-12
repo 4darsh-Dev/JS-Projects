11bf06b44393b6fb7f0c28df416bddf8f4ea4bcb
ADARSH MAURYA <109789509+4darsh-Dev@users.noreply.github.com>
2023-05-12 20:53:50 +0530

    Update snak_ water_gun.js
    Added game over functions

--- snak_ water_gun.js
+++ snak_ water_gun.js
@@ -17,6 +17,9 @@ let welcomeMsg = "********Welcome to Snake Water Gun Game*******";
 console.log(welcomeMsg);
 console.log("Snake = s, Water = w, Gun = g, Quit = q ");
 
+let userScore = 0;
+let compScore = 0;
+
 do{
     // Choses random index for respective array elements 
     let rand = Math.random();
@@ -32,24 +35,30 @@ do{
     }
     else if( userChoice == 's' && randChoice == 'w'){
         console.log("You Won! ");
+        userScore ++;
     }
     else if( userChoice == 's' && randChoice == 'g'){
         console.log("You Lost! ");
+        compScore ++;
     }
     else if( userChoice == 'w' && randChoice == 's'){
         console.log("You Lost! ");
+        compScore ++;
     }
     else if( userChoice == 'w' && randChoice == 'w'){
         console.log("Match Drawn! ");
     }
     else if( userChoice == 'w' && randChoice == 'g'){
         console.log("You won! ");
+        userScore ++;
     }
     else if( userChoice == 'g' && randChoice == 's'){
         console.log("You Won! ");
+        userScore ++;
     }
     else if( userChoice == 'g' && randChoice == 'w'){
         console.log("You Lost! ");
+        compScore ++;
     }
     else if( userChoice == 'g' && randChoice == 'g'){
         console.log("Match Drawn ! ");
@@ -61,4 +70,18 @@ do{
     
 
 }while(userChoice != 'q');
+
+console.log("\nGame Over!");
+console.log(`Your Score - ${userScore} Comp Score - ${compScore} `);
+
+// Checking the overall winner of the Game
+if(userScore < compScore){
+    console.log('You Lost');
+}
+else if(userScore > compScore){
+    console.log('You Won ');
+}
+else{
+    console.log('Match Drawn ');
+}
 
